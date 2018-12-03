5bffc237533600fc378be260b9e1d4f76f793ff4
TobiasHeimboeck <tobiasheimboeck@outlook.com>
2018-12-03 12:56:38 +0100

    Currently working on the statistics object.

---
+++ js/calculation.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright Notice
+ * Created by Tobias Heimböck
+ * Last edited 3.12.2018
+ */
+
+let content = data.results[0].members; // get all members from the json data
+
+var statistics = [
+    {
+        "Number of Democrates": 0,
+        "Number of Republicans": 0,
+        "Number if Independents": 0,
+        "Avarage 'Votes with party' for Democrates": 0,
+        "Avarage 'Votes with party' for Republicans": 0,
+        "Avarage 'Votes with party' for Independents": 0,
+        "Least engaged names": 0,
+        "Least engaged number of missed votes": 0,
+        "Least engaged percentage tableLeastEngagedmissed votes": 0,
+        "Most engaged names": 0,
+        "Most engaged number if missed votes": 0,
+        "Most engaged percentage of missed votes": 0,
+        "Least loyal names": 0,
+        "Least loyal number of votes": 0,
+        "Least loyal percentage party votes": 0,
+        "Most loyal names": 0,
+        "Most loyal number of votes": 0,
+        "Most loyal percentage party votes": 0,
+    }
+];
+
+cachePartyMemberCounts();
+
+function calculatePartyMembers() {
+    var democratesCount = [];
+    var republicanCount = [];
+    var independentsCount = [];
+
+    for (var i = 0; i < content.length; i++) {
+        var senator = content[i];
+        if (senator.party === "D") {
+            democratesCount.push(senator);
+        } else if (senator.party === "R") {
+            republicanCount.push(senator);
+        } else if (senator.party === "I") {
+            independentsCount.push(senator);
+        }
+    }
+
+    return democratesCount.length + ":" + republicanCount.length + ":" + independentsCount.length;
+}
+
+function cachePartyMemberCounts() {
+    statistics[0]["Number of Democrates"] = getCountOfParty("D");
+    statistics[0]["Number of Republicans"] = getCountOfParty("R");
+    statistics[0]["Number if Independents"] = getCountOfParty("I");
+}
+
+function getCountOfParty(party) {
+    var contentToFilter = content;
+    var partyMembers = [];
+
+    for (var i = 0; i < contentToFilter.length; i++) {
+        var senator = contentToFilter[i];
+        if (senator.party === party) {
+            partyMembers.push(senator);
+        }
+    }
+
+    return partyMembers.length;
+}
