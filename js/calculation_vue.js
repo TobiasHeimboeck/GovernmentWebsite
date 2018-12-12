41f8aac6ba7bf413fd177e4539276468e215315c
TobiasHeimboeck <tobiasheimboeck@outlook.com>
2018-12-12 11:40:39 +0100

    Currently working on the vue components

--- js/calculation_vue.js
+++ js/calculation_vue.js
@@ -1,161 +1,166 @@
 let data;
 
-findValidPage();
-
-function findValidPage() {
-    switch (location.href.split("/").slice(-1).toString()) {
-        case "senate-party-loyalty.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
-            break;
-        case "senate-attendance.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
-            break;
-        case "house-attendance.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
-            break;
-        case "house-party-loyalty.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
-            break;
-    }
-}
-
-function startFetchingAsync(url) {
-    fetch(url, {
-        method: "GET",
-        headers: new Headers({
-            "X-API-Key": "adZUIoKPgkk0ecKXE0ztm9ErLNJgARlsKHBhTBYa"
-        })
-    }).then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        data = json;
-
-        new Vue({
-            el: "#main",
-            data: {
-                members: data.results[0].members,
-                statistics: {},
-            },
-            created() {
-
-                this.loader();
-
-                var democrats = this.getSenatorsWithSameParty("D");
-                var republicans = this.getSenatorsWithSameParty("R");
-                var independents = this.getSenatorsWithSameParty("I");
-
-                var d_av = this.getAverageVotes(democrats);
-                var r_av = this.getAverageVotes(republicans);
-                var i_av = this.getAverageVotes(independents);
-
-                var total_average;
-
-                if (independents.length === 0) {
-                    total_average = Math.round(((d_av + r_av + i_av) / 2) * 100) / 100 + "%";
-                } else {
-                    total_average = Math.round(((d_av + r_av + i_av) / 3) * 100) / 100 + "%";
-                }
-
-                this.statistics = {
-                    democrats_count: democrats.length,
-                    republicans_count: republicans.length,
-                    independents_count: independents.length,
-                    democrats_average_votes_with_party: this.getAverageVotes(democrats),
-                    republicans_average_votes_with_party: this.getAverageVotes(republicans),
-                    independents_average_votes_with_party: this.getAverageVotes(independents),
-                    least_loyal_members: this.getTenPercentOfVoters("lowest"),
-                    most_loyal_members: this.getTenPercentOfVoters("highest"),
-                    least_engaged_members: this.getTenPercentOfMissedVotes("top"),
-                    most_engaged_members: this.getTenPercentOfMissedVotes("bottom"),
-                    total_count: democrats.length + republicans.length + independents.length,
-                    total_average_votes_with_party: total_average,
-
-                }
-            },
-            methods: {
-                getAverageVotes(party) {
-                    var average = 0;
-
-                    for (i = 0; i < party.length; i++)
-                        average += party[i].votes_with_party_pct;
-
-                    if (party.length === 0) {
-                        return 0;
-                    } else {
-                        return Math.round(average / party.length * 100) / 100;
-                    }
-                },
-                getSenatorsWithSameParty(party) {
-                    var senatorsWithSameParty = [];
-                    for (var i = 0; i < this.members.length; i++) {
-                        if (this.members[i].party === party)
-                            senatorsWithSameParty.push(this.members[i]);
-                    }
-                    return senatorsWithSameParty;
-                },
-                getTenPercentOfVoters(type) {
-                    var votes = [];
-                    var tenPercent = [];
-
-                    switch (type) {
-                        case "bottom":
-                            this.members.sort(function (a, b) {
-                                return a.missed_votes_pct - b.missed_votes_pct;
-                            });
-                            return this.sortingContent(tenPercent, votes);
-                        case "lowest":
-                            this.members.sort(function (a, b) {
-                                return a.votes_with_party_pct - b.votes_with_party_pct;
-                            });
-                            return this.sortingContent(tenPercent, votes);
-                        case "highest":
-                            this.members.sort(function (a, b) {
-                                return b.votes_with_party_pct - a.votes_with_party_pct;
-                            });
-                            return this.sortingContent(tenPercent, votes);
-                    }
-                },
-                getTenPercentOfMissedVotes(type) {
-                    var votes = [];
-                    var responseArray = [];
-
-                    switch (type) {
-                        case "bottom":
-                            this.members.sort(function (a, b) {
-                                return a.missed_votes_pct - b.missed_votes_pct;
-                            });
-                            return this.sortingContent(responseArray, votes);
-
-                        case "top":
-                            this.members.sort(function (a, b) {
-                                return b.missed_votes_pct - a.missed_votes_pct;
-                            });
-                            return this.sortingContent(responseArray, votes);
-                    }
-                },
-                sortingContent(array, votesArray) {
-                    for (var i = 0; i < this.members.length; i++)
-                        votesArray.push(this.members[i]);
-
-                    for (var i = 0; i < votesArray.length; i++) {
-                        if (i < ((votesArray.length) * 0.1))
-                            array.push(votesArray[i]);
-                        else if (votesArray[i] === votesArray[i - 1])
-                            array.push(votesArray[i]);
-                    }
-                    return array;
-                },
-                loader() {
-                    myVar = setTimeout(this.showPage, 500);
-                },
-                showPage() {
-                    document.getElementById("loader").style.display = "none";
-                    document.getElementById("myDiv").style.display = "block";
-                }
+var main = new Vue({
+    el: "#main",
+    data: {
+        members: [],
+        allMembers: [],
+        democrats: [],
+        republicans: [],
+        independents: [],
+        d_average: 0,
+        r_average: 0,
+        i_average: 0,
+        total_average: 0,
+        statistics: {},
+    },
+    created() {
+        this.findValidPage();
+        this.loader();
+    },
+    methods: {
+        findValidPage() {
+            switch (location.href.split("/").slice(-1).toString()) {
+                case "senate-party-loyalty.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
+                    break;
+                case "senate-attendance.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
+                    break;
+                case "house-attendance.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
+                    break;
+                case "house-party-loyalty.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
+                    break;
             }
-        });
+        },
+        startFetchingAsync(url) {
+            fetch(url, {
+                method: "GET",
+                headers: new Headers({
+                    "X-API-Key": "adZUIoKPgkk0ecKXE0ztm9ErLNJgARlsKHBhTBYa"
+                })
+            }).then(function (response) {
+                return response.json();
+            }).then(function (json) {
 
-    }).catch(function (error) {
-        console.log(error);
-    })
-}
+                data = json;
+                main.members = data.results[0].members;
+                main.allMembers = data.results[0].members;
+
+                main.democrats = main.getSenatorsWithSameParty("D");
+                main.republicans = main.getSenatorsWithSameParty("R");
+                main.independents = main.getSenatorsWithSameParty("I");
+
+                main.d_average = main.getAverageVotes(main.democrats);
+                main.r_average = main.getAverageVotes(main.republicans);
+                main.i_average = main.getAverageVotes(main.independents);
+
+                if (main.independents.length === 0) {
+                    main.total_average = Math.round(((main.d_average + main.r_average + main.i_average) / 2) * 100) / 100 + "%";
+                } else {
+                    main.total_average = Math.round(((main.d_average + main.r_average + main.i_average) / 3) * 100) / 100 + "%";
+                }
+
+                main.statistics = {
+                    democrats_count: main.democrats.length,
+                    republicans_count: main.republicans.length,
+                    independents_count: main.independents.length,
+                    democrats_average_votes_with_party: main.getAverageVotes(main.democrats),
+                    republicans_average_votes_with_party: main.getAverageVotes(main.republicans),
+                    independents_average_votes_with_party: main.getAverageVotes(main.independents),
+                    least_loyal_members: main.getTenPercentOfVoters("lowest"),
+                    most_loyal_members: main.getTenPercentOfVoters("highest"),
+                    least_engaged_members: main.getTenPercentOfMissedVotes("top"),
+                    most_engaged_members: main.getTenPercentOfMissedVotes("bottom"),
+                    total_count: main.democrats.length + main.republicans.length + main.independents.length,
+                    total_average_votes_with_party: main.total_average,
+
+                }
+
+            }).catch(function (error) {
+                console.log(error);
+            })
+        },
+        loader() {
+            myVar = setTimeout(this.showPage, 500);
+        },
+        showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        },
+        getAverageVotes(party) {
+            var average = 0;
+
+            for (i = 0; i < party.length; i++)
+                average += party[i].votes_with_party_pct;
+
+            if (party.length === 0) {
+                return 0;
+            } else {
+                return Math.round(average / party.length * 100) / 100;
+            }
+        },
+        getSenatorsWithSameParty(party) {
+            var senatorsWithSameParty = [];
+            for (var i = 0; i < this.members.length; i++) {
+                if (this.members[i].party === party)
+                    senatorsWithSameParty.push(this.members[i]);
+            }
+            return senatorsWithSameParty;
+        },
+        getTenPercentOfVoters(type) {
+            var votes = [];
+            var tenPercent = [];
+
+            switch (type) {
+                case "bottom":
+                    this.members.sort(function (a, b) {
+                        return a.missed_votes_pct - b.missed_votes_pct;
+                    });
+                    return this.sortingContent(tenPercent, votes);
+                case "lowest":
+                    this.members.sort(function (a, b) {
+                        return a.votes_with_party_pct - b.votes_with_party_pct;
+                    });
+                    return this.sortingContent(tenPercent, votes);
+                case "highest":
+                    this.members.sort(function (a, b) {
+                        return b.votes_with_party_pct - a.votes_with_party_pct;
+                    });
+                    return this.sortingContent(tenPercent, votes);
+            }
+        },
+        getTenPercentOfMissedVotes(type) {
+            var votes = [];
+            var responseArray = [];
+
+            switch (type) {
+                case "bottom":
+                    this.members.sort(function (a, b) {
+                        return a.missed_votes_pct - b.missed_votes_pct;
+                    });
+                    return this.sortingContent(responseArray, votes);
+
+                case "top":
+                    this.members.sort(function (a, b) {
+                        return b.missed_votes_pct - a.missed_votes_pct;
+                    });
+                    return this.sortingContent(responseArray, votes);
+            }
+        },
+        sortingContent(array, votesArray) {
+            for (var i = 0; i < this.members.length; i++)
+                votesArray.push(this.members[i]);
+
+            for (var i = 0; i < votesArray.length; i++) {
+                if (i < ((votesArray.length) * 0.1))
+                    array.push(votesArray[i]);
+                else if (votesArray[i] === votesArray[i - 1])
+                    array.push(votesArray[i]);
+            }
+            return array;
+        },
+    },
+});
--- js/main_vue.js
+++ js/main_vue.js
@@ -1,85 +1,93 @@
-let data;
+let data; // represents the data from the ajax api
 
-findValidPage();
+var main = new Vue({
+    el: "#main",
+    data: {
+        members: [],
+        allMembers: [],
+        checkBoxes: [],
+        checkboxDemocrat: null,
+        checkboxRepublican: null,
+        checkboxIndependent: null,
+    },
+    created() {
+        this.findValidPage();
+        this.loader();
+        this.generateStatesList();
+    },
+    methods: {
+        findValidPage() {
+            switch (location.href.split("/").slice(-1).toString()) {
+                case "senate-data.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
+                    break;
+                case "house-data.html":
+                    this.startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
+                    break;
+            }
+        },
+        startFetchingAsync(url) {
+            fetch(url, {
+                method: "GET",
+                headers: new Headers({
+                    "X-API-Key": "adZUIoKPgkk0ecKXE0ztm9ErLNJgARlsKHBhTBYa"
+                })
+            }).then(function (response) {
+                return response.json();
+            }).then(function (json) {
 
-function findValidPage() {
-    switch (location.href.split("/").slice(-1).toString()) {
-        case "senate-data.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/senate/members.json");
-            break;
-        case "house-data.html":
-            startFetchingAsync("https://api.propublica.org/congress/v1/113/house/members.json");
-            break;
-    }
-}
+                data = json;
+                main.members = data.results[0].members;
+                main.allMembers = data.results[0].members;
+                main.loadCheckboxes()
+            }).catch(function (error) {
+                console.log(error);
+            })
+        },
+        loadCheckboxes() {
+            this.checkboxDemocrat = this.$refs.democrat_check;
+            this.checkboxRepublican = this.$refs.republican_check;
+            this.checkboxIndependent = this.$refs.independent_check;
+        },
+        filterSenators() {
 
-function startFetchingAsync(url) {
-    fetch(url, {
-        method: "GET",
-        headers: new Headers({
-            "X-API-Key": "adZUIoKPgkk0ecKXE0ztm9ErLNJgARlsKHBhTBYa"
-        })
-    }).then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        data = json;
+            var senatorsArray = [];
 
-        new Vue({
-            el: "#main",
-            data: {
-                members: data.results[0].members,
-                allMembers: data.results[0].members,
-                checkboxDemocrat: document.getElementById("democrat-check"),
-                checkboxRepublican: document.getElementById("republican-check"),
-                checkboxIndependent: document.getElementById("independent-check"),
-                dropdownStates: document.getElementById("states-dropdown"),
-            },
-            created() {
-                this.loader();
-                this.generateStatesList();
-            },
-            methods: {
-                filterSenators() {
-                    var senatorsArray = [];
+            for (var i = 0; i < this.allMembers.length; i++) {
 
-                    for (var i = 0; i < this.allMembers.length; i++) {
+                if (document.getElementById("state-filter").value === this.allMembers[i].state || document.getElementById("state-filter").value === "all") {
 
-                        if (document.getElementById("state-filter").value === this.allMembers[i].state || document.getElementById("state-filter").value === "all") {
-
-                            if (document.getElementById("democrat-check").checked && this.allMembers[i].party === "D") {
-                                senatorsArray.push(this.allMembers[i]);
-                            } else if (document.getElementById("republican-check").checked && this.allMembers[i].party === "R") {
-                                senatorsArray.push(this.allMembers[i]);
-                            } else if (document.getElementById("independent-check").checked && this.allMembers[i].party === "I") {
-                                senatorsArray.push(this.allMembers[i]);
-                            }
-                        }
+                    if (document.getElementById("democrat_check").checked && this.allMembers[i].party === "D") {
+                        senatorsArray.push(this.allMembers[i]);
+                    } else if (document.getElementById("republican_check").checked && this.allMembers[i].party === "R") {
+                        senatorsArray.push(this.allMembers[i]);
+                    } else if (document.getElementById("independent_check").checked && this.allMembers[i].party === "I") {
+                        senatorsArray.push(this.allMembers[i]);
                     }
-
-                    this.members = senatorsArray;
-
-                },
-                generateStatesList() {
-                    var statesArray = [];
-
-                    for (var i = 0; i < this.allMembers.length; i++)
-                        statesArray.push(this.allMembers[i].state);
-
-                    this.states = statesArray.filter((v, i, a) => a.indexOf(v) === i).sort();
-                },
-                loader() {
-                    myVar = setTimeout(this.showPage, 500);
-                },
-                showPage() {
-                    document.getElementById("loader").style.display = "none";
-                    document.getElementById("myDiv").style.display = "block";
                 }
             }
-        });
-    }).catch(function (error) {
-        console.log(error);
-    })
-}
+
+            this.members = senatorsArray;
+
+        },
+        generateStatesList() {
+            var statesArray = [];
+
+            for (var i = 0; i < this.allMembers.length; i++)
+                statesArray.push(this.allMembers[i].state);
+
+            this.states = statesArray.filter((v, i, a) => a.indexOf(v) === i).sort();
+
+        },
+        loader() {
+            myVar = setTimeout(this.showPage, 500);
+        },
+        showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        }
+    }
+});
 
 scrollFunction();
 
